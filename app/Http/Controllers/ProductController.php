55284035450b36823cff436a81851f73f0f9db22
oussamacht <oussama_chteoui@hotmail.com>
2018-08-18 00:14:11 +0100

    Destroy Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,7 +95,8 @@ class ProductController extends Controller
        unset($request['description']);
        $product->update($request->all());
        return response([
-        'data'=>new ProductResource($product)],Response::HTTP_CREATED);
+        'data'=>new ProductResource($product)]
+        ,Response::HTTP_CREATED);
     }
 
     /**
@@ -106,6 +107,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
